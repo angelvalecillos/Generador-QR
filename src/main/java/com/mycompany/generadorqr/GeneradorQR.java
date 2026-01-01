4f3894a2a2ba8f22744894937ca454dfc9b2bf31
angelvalecillos <angel.valecillos1981@gmail.com>
2026-01-01 15:33:54 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/generadorqr/GeneradorQR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.generadorqr;
+
+/**
+ *
+ * @author angel
+ */
+public class GeneradorQR {
+
+    public static void main(String[] args) {
+        new QRgui().setVisible(true);
+    }
+}
